27712190c67faaca714a7ddf2bb7fb1f7d2c50cc
RASHID AL ANAZI <alanazirashid4@gmail.com>
2025-01-12 12:53:38 +0300

    fix: update component imports to match casing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import Header from './components/Header';
-import HomePage from './components/HomePage';
-import SecondPage from './components/SecondPage';
-import Footer from './components/Footer';
+import Header from './Components/Header';
+import HomePage from './Components/HomePage';
+import SecondPage from './Components/SecondPage';
+import Footer from './Components/Footer';
 
 function App() {
   return (
